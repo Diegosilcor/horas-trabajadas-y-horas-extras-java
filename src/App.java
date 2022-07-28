f5967424d790eb8183a7049e12596942f6e578f1
Diego Silva Cordoba <diegosilvacordoba@hotmail.com>
2022-07-28 16:04:53 +0200

    first commit

--- src/App.java
+++ src/App.java
@@ -1,29 +1,31 @@
 import javax.swing.JOptionPane;
 
-// Pedir un numero entre 0 y 99999 y decir cuantas cifras tiene
+// Pedir el dia, mes y año de una fecha e indicar si la fecha es correcta. Suponiendo que todos los meses son de 30 dias.
 
 import javax.swing.JOptionPane;
 public class App {
     public static void main(String[] args) {
-        int numero;
-
-        numero = Integer.parseInt(JOptionPane.showInputDialog("Digite un numero: "));
-
-        if (numero < 10) {
-            JOptionPane.showMessageDialog(null, "El numero  " + numero + " tiene 1 cifra");
-        }
-        else if (numero < 100) {
-            JOptionPane.showMessageDialog(null, "El numero  " + numero + " tiene 2 cifras ");
-        }
-        else if (numero < 1000)
-            JOptionPane.showMessageDialog(null, "El numero  " + numero + " tiene 3 cifras");
+        int dia, mes, año;
         
-        else if (numero < 10000)
-            JOptionPane.showMessageDialog(null, "El numero  " + numero + " tiene 4 cifras");
-        else if (numero < 100000)
-            JOptionPane.showMessageDialog(null, "El numero  " + numero + " tiene 5 cifras");    
+        dia = Integer.parseInt(JOptionPane.showInputDialog("Digite el dia: "));
+        mes = Integer.parseInt(JOptionPane.showInputDialog("Digite el mes: "));
+        año = Integer.parseInt(JOptionPane.showInputDialog("Digite el año: "));
         
+        if ((dia >= 1) && (dia <= 30)) { // Si el dia es correcto
+            if ((mes >= 1) && (mes <= 12)) { // Si el mes es correcto
+                if (año != 0) { // Si el año es correcto
+                }
+                
+                else {
+                    JOptionPane.showMessageDialog(null, "Fecha Incorrecta, año incorrecto");
+                }
+
+            } else {
+                JOptionPane.showMessageDialog(null, "Fecha Incorrecta, mes incorrecto");
+            }
+        }
+        else {
+            JOptionPane.showMessageDialog(null, "Fecha Incorrecta, dia incorrecto");
     }
-    
-    
+    }
 }
