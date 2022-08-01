538cf6402d413e7c7a3ba9f4694092f88a8257e8
Diego Silva Cordoba <diegosilvacordoba@hotmail.com>
2022-08-01 15:20:45 +0200

    First commit

--- src/App.java
+++ src/App.java
@@ -1,32 +1,52 @@
 import javax.swing.JOptionPane;
 
-// Pedir el dia, mes y año de una fecha e indicar si la fecha es correcta.Con meses de 28, 30y 31 dias. Sin años bisiestos.
+// Contruir un programa que simule el funcionamiento de una calculadora que puede realizar las cuatro operacion aritmeticas basicas (suma, resta, producto, division) con valores numericos entereos. El usuario debe especificar la operacion con el primer caracter del primer parametro de la linea de comando: S o s para la suma R o r para la resta, P, p, M o m para el producto y D o d para la division.
 
 import javax.swing.JOptionPane;
 public class App {
     public static void main(String[] args) {
-        int dia, mes, año;
+        int numero1, numero2, suma, resta, multiplicacion, division;
+        char operacion;
 
-        dia = Integer.parseInt(JOptionPane.showInputDialog("Digite el dia"));
-        mes = Integer.parseInt(JOptionPane.showInputDialog("Digite el mes"));
-        año = Integer.parseInt(JOptionPane.showInputDialog("Digite el año"));
         
-        if (año == 0) {
-            JOptionPane.showMessageDialog(null, "Fecha incorrecta");
-        }
-        else if ((mes == 2) && (dia >= 1 && dia <= 28)) {
-            JOptionPane.showMessageDialog(null, "Fecha correcta");
-        }
-        else if ((mes == 4 || mes == 6 || mes == 9 || mes == 11) && (dia >= 1 && dia <= 30)) {
-            JOptionPane.showMessageDialog(null, "Fecha correcta");
-        }
-        else if ((mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes ==10 || mes ==12) && (dia >= 1 && dia <= 31)) {
-            JOptionPane.showMessageDialog(null, "Fecha correcta");
-        }
-        else {
-            JOptionPane.showMessageDialog(null, "Fecha incorrecta");
-        }
+
+        numero1 = Integer.parseInt(JOptionPane.showInputDialog("Digite un numero"));
+        numero2 = Integer.parseInt(JOptionPane.showInputDialog("Digite un numero"));
+        
+
+        operacion = JOptionPane.showInputDialog("Digite la operacion que dese realizar").charAt(0);
+
+
+        switch (operacion) {
+            case 's':
+            case 'S':
+                suma = numero1 + numero2;
+                JOptionPane.showMessageDialog(null, "La suma es: " + suma);
+                break;
+
+            case 'r':
+            case 'R':
+                resta = numero1 - numero2;
+                JOptionPane.showMessageDialog(null, "La resta es: " + resta);
+                break;
+
+            case 'p':
+            case 'P':
+            case 'M':
+            case 'm':
+                multiplicacion = numero1 * numero2;
+                JOptionPane.showMessageDialog(null, "La multiplicacion es: " + multiplicacion);
+                break;
             
+            case 'D':
+            case 'd':
+                division = numero1/numero2;
+                JOptionPane.showMessageDialog(null, "La division es: " + division);
+                break;
+                
+            default: JOptionPane.showMessageDialog(null, "Error, se equivoco de operacion");  
+            
+            }            
         }
     }
 
